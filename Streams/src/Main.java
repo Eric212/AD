bac05ee4f2dfe7d388847c20fa050764bfb71d83
Eric_212 <ericsospedrasalort212@gmail.com>
2023-09-18 19:22:15 +0200

    Ficheros

---
+++ Streams/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        File ficheros = new File("ficheros");
+        if(!ficheros.exists()) {
+            ficheros.mkdir();
+        }else {
+            System.err.println("El directorio ya existe");
+        }
+        File file = new File(ficheros.getPath(),"caracteres.txt");
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file);
+        }catch (IOException ex){
+            System.err.println("Error de apertura en el fichero"+file.getName());
+        }
+        try {
+            fileWriter.write("Esto es un texto de prueba");
+        }catch (IOException ex){
+            System.err.println("Error de escritura sobre el fichero"+file.getName());
+        }
+        try{
+            fileWriter.close();
+        }catch (IOException ex){
+            System.err.println("Error al guardar las modificaciones en el fichero"+file.getName());
+        }
+
+        try{
+            FileReader fileReader = new FileReader(file);
+            int read;
+            while ((read=fileReader.read())!=-1){
+                System.out.print((char)read);
+            }
+            System.out.println();
+        } catch (IOException e) {
+            System.err.println("Error de lectura del archivo"+file.getName());
+        }
+    }
+}
